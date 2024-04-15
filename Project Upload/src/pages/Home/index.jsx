1797baa2c76af8b2c73147713282b58ba71e2ed0
Muhammad Danish Khan <69090543+MuhammadDanishKhan16259@users.noreply.github.com>
2024-04-15 20:36:08 +0500

    Add files via upload

---
+++ Project Upload/src/pages/Home/index.jsx
@@ -0,0 +1,43 @@
+import LOGO from "../../assets/logo.svg"
+import "./css/style.scss";
+import { MdOutlineTextFields } from "react-icons/md";
+import { FaRegFileAlt } from "react-icons/fa";
+
+function HomePage() {
+  return (
+    <div className="container">
+      <div className="header-bar">
+        <div className="logo">
+          <img src={LOGO} alt="" />
+        </div>
+        <div className="menu-bar">
+          <ul>
+            <li>How it works</li>
+            <li>Download</li>
+            <li>Upgrade </li>
+            <li>Feedback</li>
+            <li className="menu-btn">Login / Register</li>
+          </ul>
+        </div>
+      </div>
+      <div className="main-card">
+      <div className="card-sidebar">
+      <div className="active">
+      <MdOutlineTextFields size={40}/>
+      </div>
+      <div>
+      <FaRegFileAlt size={35}/>
+      </div>
+   <div>
+   
+   </div>
+      </div>
+      <div className="card-container">
+      
+      </div>
+      </div>
+    </div>
+  );
+}
+
+export default HomePage;
